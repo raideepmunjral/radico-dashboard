1fd52d7c7bcd88abe1ed2bbe862935637a64a9bd
raideepmunjral <raideep@munjral.com>
2025-05-27 15:31:43 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,8 +7,8 @@ const nextConfig = {
   },
   env: {
     NEXT_PUBLIC_GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
-    NEXT_PUBLIC_MASTER_SHEET_ID: '1pRz9CgOoamTrfpnmF-XuBCg9IZON9br5avgRlKYtxM',
-    NEXT_PUBLIC_VISIT_SHEET_ID: '1XG4c_Lrpk-YglTq3G3ZY9Qjt7wSnUq0UZWDSYT61eWt'
+    NEXT_PUBLIC_MASTER_SHEET_ID: '1pRz9CgOoamTrFipnmF-XuBCg9IZON9br5avgRlKYtxM',
+    NEXT_PUBLIC_VISIT_SHEET_ID: '1XG4c_Lrpk-YglTq3G3ZY9Qjt7wSnUq0UZWDSYT61eWE',
   },
   // Suppress build warnings for better performance
   typescript: {
