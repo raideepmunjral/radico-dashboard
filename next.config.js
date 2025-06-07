533c765f72564bfb0c3e95d04ad79f909fb10b62
raideepmunjral <raideep@munjral.com>
2025-06-07 12:56:11 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   distDir: 'out',
   env: {
     NEXT_PUBLIC_GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
-    NEXT_PUBLIC_MASTER_SHEET_ID: '1pRz9CgOoamTrFipnmF-XuBCg9IZON9br5avgRlKYtxM',
+    NEXT_PUBLIC_MASTER_SHEET_ID: '1pRz9CgOoamTrfpnmF-XuBCg9IZON9br5avgRlKYtxM',
     NEXT_PUBLIC_VISIT_SHEET_ID: '1XG4c_Lrpk-YglTq3G3ZY9Qjt7wSnUq0UZWDSYT61eWE',
     NEXT_PUBLIC_HISTORICAL_SHEET_ID: '1yXzEYHJeHlETrEmU4TZ9F2_qv4OE10N4DPdYX0Iqfx0',
   },
@@ -21,5 +21,4 @@ const nextConfig = {
     ignoreDuringBuild: false,
   }
 }
-
 module.exports = nextConfig
