33dd6ab1494f3e5fbb859c5fc696acac56acf6b5
raideepmunjral <raideep@munjral.com>
2025-06-07 11:44:32 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true,
+  trailingSlash: true,  // Add this line
   basePath: '/radico-dashboard',
   assetPrefix: '/radico-dashboard',
   images: {
@@ -11,10 +11,9 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
     NEXT_PUBLIC_MASTER_SHEET_ID: '1pRz9CgOoamTrFipnmF-XuBCg9IZON9br5avgRlKYtxM',
-    NEXT_PUBLIC_VISIT_SHEET_ID: '1XG4c_Lrpk-YglTq3G3ZY9Qjt7wSnUq0UZWDSYT61eWE',
+    NEXT_PUBLIC_VISIT_SHEET_ID: '1XG4c_Lrpk-YglTq3G3ZY9F2_qv4OE10N4DPdYX0Iqfx0',
     NEXT_PUBLIC_HISTORICAL_SHEET_ID: '1yXzEYHJeHlETrEmU4TZ9F2_qv4OE10N4DPdYX0Iqfx0',
   },
-  // Suppress build warnings for better performance
   typescript: {
     ignoreBuildErrors: false,
   },
