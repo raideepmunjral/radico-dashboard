40e0c90a9d881ae25238d9af2c1d217d888053a9
raideepmunjral <raideep@munjral.com>
2025-07-04 08:15:10 +0530

    Update DepartmentTab.tsx

--- components/tabs/sales/DepartmentTab.tsx
+++ components/tabs/sales/DepartmentTab.tsx
@@ -501,7 +501,7 @@ const DepartmentTab = ({ data }: { data: DashboardData }) => {
 
     const { department, title, subtitle, shops, type, monthData, brandType } = selectedDepartmentShops;
 
-    function getTypeStyles(type: string) {
+    const getTypeStyles = (type: string) => {
       if (type.includes('8PM')) {
         return { bg: 'bg-purple-50', text: 'text-purple-600', border: 'border-purple-200' };
       } else if (type.includes('VERVE')) {
@@ -526,7 +526,7 @@ const DepartmentTab = ({ data }: { data: DashboardData }) => {
         case 'volatile': return { bg: 'bg-amber-50', text: 'text-amber-600', border: 'border-amber-200' };
         default: return { bg: 'bg-gray-50', text: 'text-gray-600', border: 'border-gray-200' };
       }
-    }
+    };
 
     const typeStyles = getTypeStyles(type);
 
